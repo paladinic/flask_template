e1cf9a86e3d77bca2b09f2c0d6ea9812d7897f35
Claudio Paladini# <clode.paladini@gmail.com>
2020-11-13 13:10:40 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, url_for, render_template
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+# index
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+# index
+@app.route("/page2", methods=['GET', 'POST'])
+def page2():
+    return render_template('page2.html')
