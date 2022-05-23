6100ea057faf3190fedd4664dcd7add747a32bab
Claudio Paladini <clode.paladini@gmail.com>
2022-05-23 05:56:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,8 @@ def index():
 @app.route("/page2", methods=['GET', 'POST'])
 def page2():
     return render_template('page2.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port = port, host="0.0.0.0")
+
